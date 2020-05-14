a041724f00025941bc533bbe5c83b82730ccfa60
Alessio Matricardi <alessiomatricardi@yahoo.com>
2020-05-14 16:18:02 +0200

    modifica

--- src/direttore.c
+++ src/direttore.c
@@ -3,5 +3,9 @@
 
 void* direttore(void* arg) {
     direttore_opt_t direttore = *((direttore_opt_t*)arg);
-
+    while(1) {
+        // controlla stato
+        // controlla notifiche - chiudi casse
+        // broadcast 
+    }
 }
